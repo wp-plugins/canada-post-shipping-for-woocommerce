6d27fd4e78d3772058cce97a04e585adacf6a5d7
canship <canship@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-01 04:39:52 +0000

    initial commit
    git-svn-id: https://plugins.svn.wordpress.org/canada-post-shipping-for-woocommerce/trunk@1035777 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ canadapost.php
@@ -0,0 +1,654 @@
+<?php
+/**
+* Plugin Name: Canada Post Shipping For WooCommerce By CanShip
+* Description: Adds Canada Post shipping rates to WooCommerce 
+* Version: 1.0
+* Author: CanShip
+*/
+
+if ( ! defined( 'ABSPATH' ) ) { 
+    exit; // Exit if accessed directly
+}
+
+/**
+ * Check if WooCommerce is active
+ **/
+if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+    
+	function canada_post_shipping_init() {
+			if ( ! class_exists( 'Canada_Post_Shipping_For_WooCommerce_By_CanShip' ) ) {
+				
+				class Canada_Post_Shipping_For_WooCommerce_By_CanShip extends WC_Shipping_Method {
+					
+					 /**
+					 * Constructor for your shipping class
+					 *
+					 * @access public
+					 * @return void
+					 */
+					public function __construct() {
+						$this->id                 = 'canada_post_shipping'; // Id for your shipping method. Should be uunique.
+						$this->method_title       = __( 'Canada Post Shipping' );  // Title shown in admin
+						$this->method_description = __( 'Canada post shipping lets you automatically calculate Canada post shipping rates. <br /><br /> If item sizes fit within the listed dimensions and weights then this shipping option will be presented to the buyers during the checkout. <br /><br /> These prices are standard mailing rates that are posted by Canada Post. <br /><br />If you need help or have questions please feel free to send an email to <a href="mailto:nalidixic@gmail.com">nalidixic@gmail.com.</a>' ); // Description shown in admin
+						$this->title = 'Canada Post';
+
+						$this->init();
+					}
+				
+					/**
+					 * Init your settings
+					 *
+					 * @access public
+					 * @return void
+					 */
+					function init() {
+						// Load the settings API
+						$this->init_form_fields(); // This is part of the settings API. Override the method to add your own settings
+						$this->init_settings(); // This is part of the settings API. Loads settings you previously init.
+
+						// Save settings in admin if you have any defined
+						add_action( 'woocommerce_update_options_shipping_' . $this->id, array( $this, 'process_admin_options' ) );
+					}
+					
+					public function generate_settings_html() {
+						
+						parent::generate_settings_html();
+
+						echo "<table class='wc_status_table widefat'>";
+							echo "<thead>";
+								echo "<tr>";
+									echo "<th colspan=6><h3>Letter Mail Rates</h3></th>";
+								echo "</tr>";
+								echo "<tr>";
+									echo "<th>";
+										echo "Min Weight";
+									echo "</th>";
+									echo "<th>";
+										echo "Max Weight";
+									echo "</th>";
+									echo "<th>";
+										echo "Max Width";
+									echo "</th>";
+									echo "<th>";
+										echo "Max Length";
+									echo "</th>";
+									echo "<th>";
+										echo "Max Height";
+									echo "</th>";
+									echo "<th>";
+										echo "Price";
+									echo "</th>";
+								echo "</tr>";
+							echo "</thead>";
+							echo "<tr>";
+								echo "<td>";
+									echo "0g";
+								echo "</td>";
+								echo "<td>";
+									echo "30g";
+								echo "</td>";
+								echo "<td>";
+									echo "245mm";
+								echo "</td>";
+								echo "<td>";
+									echo "156mm";
+								echo "</td>";
+								echo "<td>";
+									echo "5mm";
+								echo "</td>";
+								echo "<td>";
+									echo "$1.00";
+								echo "</td>";
+							echo "</tr>";
+							echo "<tr>";
+								echo "<td>";
+									echo "31g";
+								echo "</td>";
+								echo "<td>";
+									echo "50g";
+								echo "</td>";
+								echo "<td>";
+									echo "245mm";
+								echo "</td>";
+								echo "<td>";
+									echo "156mm";
+								echo "</td>";
+								echo "<td>";
+									echo "5mm";
+								echo "</td>";
+								echo "<td>";
+									echo "$1.20";
+								echo "</td>";
+							echo "</tr>";
+							echo "<tr>";
+								echo "<td>";
+									echo "51g";
+								echo "</td>";
+								echo "<td>";
+									echo "100g";
+								echo "</td>";
+								echo "<td>";
+									echo "380mm";
+								echo "</td>";
+								echo "<td>";
+									echo "270mm";
+								echo "</td>";
+								echo "<td>";
+									echo "20mm";
+								echo "</td>";
+								echo "<td>";
+									echo "$1.80";
+								echo "</td>";
+							echo "</tr>";
+							echo "<tr>";
+								echo "<td>";
+									echo "101g";
+								echo "</td>";
+								echo "<td>";
+									echo "200g";
+								echo "</td>";
+								echo "<td>";
+									echo "380mm";
+								echo "</td>";
+								echo "<td>";
+									echo "270mm";
+								echo "</td>";
+								echo "<td>";
+									echo "20mm";
+								echo "</td>";
+								echo "<td>";
+									echo "$2.95";
+								echo "</td>";
+							echo "</tr>";
+							echo "<tr>";
+								echo "<td>";
+									echo "201g";
+								echo "</td>";
+								echo "<td>";
+									echo "300g";
+								echo "</td>";
+								echo "<td>";
+									echo "380mm";
+								echo "</td>";
+								echo "<td>";
+									echo "270mm";
+								echo "</td>";
+								echo "<td>";
+									echo "20mm";
+								echo "</td>";
+								echo "<td>";
+									echo "$4.10";
+								echo "</td>";
+							echo "</tr>";
+							echo "<tr>";
+								echo "<td>";
+									echo "301g";
+								echo "</td>";
+								echo "<td>";
+									echo "400g";
+								echo "</td>";
+								echo "<td>";
+									echo "380mm";
+								echo "</td>";
+								echo "<td>";
+									echo "270mm";
+								echo "</td>";
+								echo "<td>";
+									echo "20mm";
+								echo "</td>";
+								echo "<td>";
+									echo "$4.70";
+								echo "</td>";
+							echo "</tr>";
+							echo "<tr>";
+								echo "<td>";
+									echo "401g";
+								echo "</td>";
+								echo "<td>";
+									echo "500g";
+								echo "</td>";
+								echo "<td>";
+									echo "380mm";
+								echo "</td>";
+								echo "<td>";
+									echo "270mm";
+								echo "</td>";
+								echo "<td>";
+									echo "20mm";
+								echo "</td>";
+								echo "<td>";
+									echo "$5.05";
+								echo "</td>";
+							echo "</tr>";
+						echo "</table>";
+						
+						echo "<br />";
+						echo "<br />";
+						
+						echo "<table class='wc_status_table widefat'>";
+							echo "<thead>";
+								echo "<tr>";
+									echo "<th colspan=6><h3>U.S.A Letter-Post Rates</h3></th>";
+								echo "</tr>";
+								echo "<tr>";
+									echo "<th>";
+										echo "Min Weight";
+									echo "</th>";
+									echo "<th>";
+										echo "Max Weight";
+									echo "</th>";
+									echo "<th>";
+										echo "Max Width";
+									echo "</th>";
+									echo "<th>";
+										echo "Max Length";
+									echo "</th>";
+									echo "<th>";
+										echo "Max Height";
+									echo "</th>";
+									echo "<th>";
+										echo "Price";
+									echo "</th>";
+								echo "</tr>";
+							echo "</thead>";
+							echo "<tr>";
+								echo "<td>";
+									echo "0g";
+								echo "</td>";
+								echo "<td>";
+									echo "30g";
+								echo "</td>";
+								echo "<td>";
+									echo "245mm";
+								echo "</td>";
+								echo "<td>";
+									echo "150mm";
+								echo "</td>";
+								echo "<td>";
+									echo "5mm";
+								echo "</td>";
+								echo "<td>";
+									echo "$1.20";
+								echo "</td>";
+							echo "</tr>";
+							echo "<tr>";
+								echo "<td>";
+									echo "31g";
+								echo "</td>";
+								echo "<td>";
+									echo "50g";
+								echo "</td>";
+								echo "<td>";
+									echo "245mm";
+								echo "</td>";
+								echo "<td>";
+									echo "150mm";
+								echo "</td>";
+								echo "<td>";
+									echo "5mm";
+								echo "</td>";
+								echo "<td>";
+									echo "$1.60";
+								echo "</td>";
+							echo "</tr>";
+							echo "<tr>";
+								echo "<td>";
+									echo "51g";
+								echo "</td>";
+								echo "<td>";
+									echo "100g";
+								echo "</td>";
+								echo "<td>";
+									echo "380mm";
+								echo "</td>";
+								echo "<td>";
+									echo "270mm";
+								echo "</td>";
+								echo "<td>";
+									echo "20mm";
+								echo "</td>";
+								echo "<td>";
+									echo "$2.95";
+								echo "</td>";
+							echo "</tr>";
+							echo "<tr>";
+								echo "<td>";
+									echo "101g";
+								echo "</td>";
+								echo "<td>";
+									echo "200g";
+								echo "</td>";
+								echo "<td>";
+									echo "380mm";
+								echo "</td>";
+								echo "<td>";
+									echo "270mm";
+								echo "</td>";
+								echo "<td>";
+									echo "20mm";
+								echo "</td>";
+								echo "<td>";
+									echo "$5.15";
+								echo "</td>";
+							echo "</tr>";
+							echo "<tr>";
+								echo "<td>";
+									echo "201g";
+								echo "</td>";
+								echo "<td>";
+									echo "500g";
+								echo "</td>";
+								echo "<td>";
+									echo "380mm";
+								echo "</td>";
+								echo "<td>";
+									echo "270mm";
+								echo "</td>";
+								echo "<td>";
+									echo "20mm";
+								echo "</td>";
+								echo "<td>";
+									echo "$10.30";
+								echo "</td>";
+							echo "</tr>";
+						echo "</table>";
+						
+						echo "<br />";
+						echo "<br />";				
+						
+						echo "<table class='wc_status_table widefat'>";
+							echo "<thead>";
+								echo "<tr>";
+									echo "<th colspan=6><h3>International Letter-Post Rates</h3></th>";
+								echo "</tr>";
+								echo "<tr>";
+									echo "<th>";
+										echo "Min Weight";
+									echo "</th>";
+									echo "<th>";
+										echo "Max Weight";
+									echo "</th>";
+									echo "<th>";
+										echo "Max Width";
+									echo "</th>";
+									echo "<th>";
+										echo "Max Length";
+									echo "</th>";
+									echo "<th>";
+										echo "Max Height";
+									echo "</th>";
+									echo "<th>";
+										echo "Price";
+									echo "</th>";
+								echo "</tr>";
+							echo "</thead>";
+							echo "<tr>";
+								echo "<td>";
+									echo "0g";
+								echo "</td>";
+								echo "<td>";
+									echo "30g";
+								echo "</td>";
+								echo "<td>";
+									echo "245mm";
+								echo "</td>";
+								echo "<td>";
+									echo "150mm";
+								echo "</td>";
+								echo "<td>";
+									echo "5mm";
+								echo "</td>";
+								echo "<td>";
+									echo "$2.50";
+								echo "</td>";
+							echo "</tr>";
+							echo "<tr>";
+								echo "<td>";
+									echo "31g";
+								echo "</td>";
+								echo "<td>";
+									echo "50g";
+								echo "</td>";
+								echo "<td>";
+									echo "245mm";
+								echo "</td>";
+								echo "<td>";
+									echo "150mm";
+								echo "</td>";
+								echo "<td>";
+									echo "5mm";
+								echo "</td>";
+								echo "<td>";
+									echo "$3.60";
+								echo "</td>";
+							echo "</tr>";
+							echo "<tr>";
+								echo "<td>";
+									echo "51g";
+								echo "</td>";
+								echo "<td>";
+									echo "100g";
+								echo "</td>";
+								echo "<td>";
+									echo "380mm";
+								echo "</td>";
+								echo "<td>";
+									echo "270mm";
+								echo "</td>";
+								echo "<td>";
+									echo "20mm";
+								echo "</td>";
+								echo "<td>";
+									echo "$5.90";
+								echo "</td>";
+							echo "</tr>";
+							echo "<tr>";
+								echo "<td>";
+									echo "101g";
+								echo "</td>";
+								echo "<td>";
+									echo "200g";
+								echo "</td>";
+								echo "<td>";
+									echo "380mm";
+								echo "</td>";
+								echo "<td>";
+										echo "270mm";
+									echo "</td>";
+								echo "<td>";
+									echo "20mm";
+								echo "</td>";
+								echo "<td>";
+									echo "$10.30";
+								echo "</td>";
+							echo "</tr>";
+							echo "<tr>";
+								echo "<td>";
+									echo "201g";
+								echo "</td>";
+								echo "<td>";
+									echo "500g";
+								echo "</td>";
+								echo "<td>";
+									echo "380mm";
+								echo "</td>";
+								echo "<td>";
+									echo "270mm";
+								echo "</td>";
+								echo "<td>";
+									echo "20mm";
+								echo "</td>";
+								echo "<td>";
+									echo "$20.60";
+								echo "</td>";
+							echo "</tr>";
+						echo "</table>";
+						
+						echo "<br />";
+						echo "<br />";
+					}
+					
+					/**
+					* init_form_fields function
+					*/
+					public function init_form_fields() {
+						$this->form_fields = array(
+								'enabled' => array(
+									'title' => __('Enable/Disable', 'woocommerce'),
+									'label' => __('Enable this shipping method', 'woocommerce'),
+									'type' => 'checkbox')
+							);
+					}
+
+					/**
+					 * calculate_shipping function.
+					 *
+					 * @access public
+					 * @param mixed $package
+					 * @return void
+					 */
+					public function calculate_shipping( $package ) {
+						
+						if (array_values($package["contents"])[0]["data"]->has_weight()
+							&& array_values($package["contents"])[0]["data"]->has_dimensions())
+						{
+							
+							$allow_shipping = true;
+							$amount = 0.00;
+							
+							// Get product dimensions
+							$weight = array_values($package["contents"])[0]["data"]->get_weight();
+							$width = array_values($package["contents"])[0]["data"]->width;
+							$height = array_values($package["contents"])[0]["data"]->height;
+							$length = array_values($package["contents"])[0]["data"]->length;
+							
+							// Organize package sizes
+							$max = max($width, $height, $length);
+							$min = min($width, $height, $length);
+							$middle = $width + $height + $length - $max - $min;
+					
+							// Check to see if standard Canada Post can handle the package
+							if ($weight > 0.5) {
+								$allow_shipping = false;
+							}
+							
+							if ($max > 38 || $max <= 0.0) {
+								$allow_shipping = false;
+							}
+							
+							if ($middle > 27 || $middle <= 0.0) {
+								$allow_shipping = false;
+							}
+							
+							if ($min > 2 || $min <= 0.0) {
+								$allow_shipping = false;
+							}
+							
+							// If it doesn't fit then don't add a shipping rate
+							if (!$allow_shipping) {
+								return;
+							}
+							
+							// Calculate the shipping
+							switch ($package["destination"]["country"]) {
+								case 'CA':
+									if ($weight <= 0.03 && $max <= 24.5 && $middle <= 15.6 && $min <= 0.5) {
+										$amount = 1.00;
+										break;
+									}
+									if ($weight <= 0.05 && $max <= 24.5 && $middle <= 15.6 && $min <= 0.5) {
+										$amount = 1.20;
+										break;
+									}
+									if ($weight <= 0.1 && $max <= 38.0 && $middle <= 27.0 && $min <= 2.0) {
+										$amount = 1.80;
+										break;
+									}
+									if ($weight <= 0.2 && $max <= 38.0 && $middle <= 27.0 && $min <= 2.0) {
+										$amount = 2.95;
+										break;
+									}
+									if ($weight <= 0.3 && $max <= 38.0 && $middle <= 27.0 && $min <= 2.0) {
+										$amount = 4.10;
+										break;
+									}
+									if ($weight <= 0.4 && $max <= 38.0 && $middle <= 27.0 && $min <= 2.0) {
+										$amount = 4.70;
+										break;
+									}
+									if ($weight <= 0.5 && $max <= 38.0 && $middle <= 27.0 && $min <= 2.0) {
+										$amount = 5.05;
+										break;
+									}
+									break;
+								case 'US':
+									if ($weight <= 0.03 && $max <= 24.5 && $middle <= 15.0 && $min <= 0.5) {
+										$amount = 1.20;
+										break;
+									}
+									if ($weight <= 0.05 && $max <= 24.5 && $middle <= 15.0 && $min <= 0.5) {
+										$amount = 1.60;
+										break;
+									}
+									if ($weight <= 0.1 && $max <= 38.0 && $middle <= 27.0 && $min <= 2.0) {
+										$amount = 2.95;
+										break;
+									}
+									if ($weight <= 0.2 && $max <= 38.0 && $middle <= 27.0 && $min <= 2.0) {
+										$amount = 5.15;
+										break;
+									}
+									if ($weight <= 0.3 && $max <= 38.0 && $middle <= 27.0 && $min <= 2.0) {
+										$amount = 10.30;
+										break;
+									}
+									break;
+								default:
+									if ($weight <= 0.03 && $max <= 24.5 && $middle <= 15.0 && $min <= 0.5) {
+										$amount = 2.50;
+										break;
+									}
+									if ($weight <= 0.05 && $max <= 24.5 && $middle <= 15.0 && $min <= 0.5) {
+										$amount = 3.60;
+										break;
+									}
+									if ($weight <= 0.1 && $max <= 38.0 && $middle <= 27.0 && $min <= 2.0) {
+										$amount = 5.90;
+										break;
+									}
+									if ($weight <= 0.2 && $max <= 38.0 && $middle <= 27.0 && $min <= 2.0) {
+										$amount = 10.30;
+										break;
+									}
+									if ($weight <= 0.3 && $max <= 38.0 && $middle <= 27.0 && $min <= 2.0) {
+										$amount = 20.60;
+										break;
+									}
+									break;
+							}
+						
+							$rate = array(
+								'id' => $this->id,
+								'label' => $this->title,
+								'cost' => $amount,
+								'calc_tax' => 'per_item'
+							);
+								
+							// Register the rate
+							$this->add_rate( $rate );
+						}
+					}
+				}
+			}
+		}
+
+		add_action( 'woocommerce_shipping_init', 'canada_post_shipping_init' );
+
+		function add_your_shipping_method( $methods ) {
+			$methods[] = 'Canada_Post_Shipping_For_WooCommerce_By_CanShip';
+			return $methods;
+		}
+
+		add_filter( 'woocommerce_shipping_methods', 'add_your_shipping_method' );
+
+}
---
+++ readme.txt
@@ -0,0 +1,15 @@
+=== Canada Post Shipping For WooCommerce By CanShip ===
+Contributors: canship
+Tags: shipping
+Requires at least: 3.0.1
+Tested up to: 4.0.1
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Adds Canada Post shipping rates to WooCommerce 
+== Description ==
+Canada Post Shipping For WooCommerce By CanShip is a plugin that adds support for Canada Post shipping to WooCommerce. After installing the plugin your customers will be offered the ability to choose Canada Post as a shipping method during the checkout.
+The plugin automatically checks the sizes of the items your customers are buying and calculates the shipping costs automatically.
+== Changelog ==
+= 1.0.0 =
+Initial Release
